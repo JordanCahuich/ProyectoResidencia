a8899618a7cb0c7ffd415ad786148aacc6897a52
GabiAke08 <114690562+GabiAke08@users.noreply.github.com>
2022-10-10 14:00:26 -0500

    registro de pago

---
+++ app/Models/Pago.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pago extends Model
+{
+    protected $table = 'pagos'; // a que tabla de la base de datos se hace referencia
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'fecha', 'vigencia','importe_total','status','tipo','photo_pago'
+    ];
+    public function disease()
+    {
+        return $this->belongsTo('App\Disease');
+    }
+    public function scopeMatriculas($query, $id) {
+        if ($nombres) {
+            return $query->where('id','like',"%id%");
+        }
+    }
+
+}
