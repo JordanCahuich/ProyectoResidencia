fa6e37ccc7b922656c5fe2846b6f4f30eaa76312
zaira20 <114689378+zaira20@users.noreply.github.com>
2023-02-23 21:00:51 -0600

    interfaz editado de la seccion docente, carga y horarios

--- resources/views/horarios/add.blade.php
+++ resources/views/horarios/add.blade.php
@@ -3,8 +3,22 @@
 @section('content')
 
 
+<div class="py-4">
+    <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+        <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+            <li class="breadcrumb-item">
+                <a href="#">
+                    <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+                </a>
+            </li>
+            <li class="breadcrumb-item"><a href="#">Horarios</a></li>
+            
+        </ol>
+    </nav>
+
+
 <div class="row justify-content-center">
-    <FONT FACE="century gothic" SIZE=4 COLOR="#212121">
+  
 <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
         <div class="card-body">
@@ -82,14 +96,14 @@
              
           
             <div class="form-group text-center">
-                <button type="submit" class="btn btn-primary mr-2 mb-3" style="background-color: 	#8cf064">Registrar</button>
-                <a href="/"  class="btn btn-inverse-dark btn-fw mb-3" style="background-color: 	#f75e5e">Cancelar</a>
+                <button type="submit" class="btn btn-primary mr-2 mb-3" >Registrar</button>
+                <a href="/"  class="btn btn-outline-gray-600 d-inline-flex align-items-center" >Cancelar</a>
             </div>
         </form>
         </div>
     </div>
 </div>
-    </FONT>
+    
 </div>
 
 @endsection
--- resources/views/profes/add.blade.php
+++ resources/views/profes/add.blade.php
@@ -3,8 +3,24 @@
 @section('content')
 
 
+<div class="py-4">
+  <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+      <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+          <li class="breadcrumb-item">
+              <a href="#">
+                  <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>
+              </a>
+          </li>
+          <li class="breadcrumb-item"><a href="#">Seccion de Docentes</a></li>
+          <li class="breadcrumb-item active" aria-current="page">Registrar docentes</li>
+      </ol>
+  </nav>
+
+
+
+  
 <div class="row justify-content-center">
-  <FONT FACE="century gothic" SIZE=4 COLOR="#212121">
+ 
 <div class="col-md-12 grid-margin stretch-card">
     <div class="card">
         <div class="card-body">
@@ -20,6 +36,8 @@
                     </div>
                 </div>
 
+
+                
                 <div class="col-md-4">
                     <label class="col-sm-6 col-form-label">Fecha de nacimiento</label>
                     <div class="col-sm-12">
@@ -85,12 +103,16 @@
             <div class="form-group text-center">
                 <button type="submit" class="btn btn-primary mr-2 mb-3" style="background-color: 	#8cf064">Registrar Docente</button>
                 <a href="/"  class="btn btn-inverse-dark btn-fw mb-3" style="background-color: 	#f75e5e">Cancelar</a>
+        
             </div>
+
+
+            
         </form>
         </div>
     </div>
 </div>
-</FONT>
+
 </div>
 
 @endsection
